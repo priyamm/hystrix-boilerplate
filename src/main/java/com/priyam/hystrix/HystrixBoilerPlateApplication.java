37f3b1e7ab8ec728bbac9d90d3ce4be582ddf18b
Priyam Gupta <priyam.gupta@abspl.in>
2018-05-23 17:23:29 +0530

    first-commit

---
+++ src/main/java/com/priyam/hystrix/HystrixBoilerPlateApplication.java
@@ -0,0 +1,12 @@
+package com.priyam.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixBoilerPlateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixBoilerPlateApplication.class, args);
+	}
+}
